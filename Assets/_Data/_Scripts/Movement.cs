3123a6a267c6113ad1d41df607debcc30747bc31
ZuForSure <anhvux20se@gmail.com>
2025-12-08 19:48:32 +0700

    Add Movement Script
    Moving with rigibody2D

---
+++ Assets/_Data/_Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float horizonInput, verticalInput;
+    public float speed = 5f;
+    public Rigidbody2D rigi2D;
+
+    void Start()
+    {
+        this.rigi2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        this.horizonInput = Input.GetAxisRaw("Horizontal");
+        this.verticalInput = Input.GetAxisRaw("Vertical");
+
+        this.Moving();
+    }
+
+    protected void Moving()
+    {
+        Vector2 direction = new(this.horizonInput, this.verticalInput);
+        direction.Normalize();
+
+        rigi2D.MovePosition(rigi2D.position + direction * this.speed * Time.fixedDeltaTime);
+    }
+}
