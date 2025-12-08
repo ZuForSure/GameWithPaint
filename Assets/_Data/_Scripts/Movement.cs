84acb720575b6f61bde7a677c1e085d1cf0e58ee
ZuForSure <anhvux20se@gmail.com>
2025-12-08 20:04:13 +0700

    Add Comment

--- Assets/_Data/_Scripts/Movement.cs
+++ Assets/_Data/_Scripts/Movement.cs
@@ -10,6 +10,7 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
+        // start game se tu dong add rigibody2D vao bien rigi2D
         this.rigi2D = GetComponent<Rigidbody2D>();
     }
 
@@ -20,6 +21,10 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // lay input di chuyen gan vao bien horizonInput, "Horizontal" "Vertical" la mac dinh cua Unity
+        // horizon la di chuyen theo truc x (1: right, -1: left), vertical la truc y (1: up, -1: down)
+        // GetAxisRaw de lay so nguyen
+
         this.horizonInput = Input.GetAxisRaw("Horizontal");
         this.verticalInput = Input.GetAxisRaw("Vertical");
 
@@ -28,6 +33,9 @@ public class Movement : MonoBehaviour
 
     protected void Moving()
     {
+        // tao 1 bien Vector2 la huong di chuyen cua player
+        // duong cheo cua hinh vuong 1, la 1.4. Dung Normalize de duong cheo la 1
+
         Vector2 direction = new(this.horizonInput, this.verticalInput);
         direction.Normalize();
 
