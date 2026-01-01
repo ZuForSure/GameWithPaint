f9ea53bf0f10400b2e31348840ddb09f5b898415
ZuForSure <anhvux20se@gmail.com>
2026-01-01 19:20:40 +0700

    Add PoolManager and IPoolable

---
+++ Assets/_Data/_Scripts/PoolManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Search;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class PoolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static PoolManager Instance;
+
+    [SerializeField] protected Pool[] pools;
+    private Dictionary<GameObject, Queue<GameObject>> poolDict;
+
+    private void Awake()
+    {
+        Instance = this;
+        poolDict = new Dictionary<GameObject, Queue<GameObject>>();
+
+        foreach (var pool in pools)
+        {
+            Queue<GameObject> poolQueue = new();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab, transform);
+                obj.SetActive(false);
+                poolQueue.Enqueue(obj);
+            }
+
+            poolDict.Add(pool.prefab, poolQueue);
+        }
+    }
+
+    public GameObject Spawn(GameObject prefab, Vector3 pos, Quaternion rot)
+    {
+        if (!poolDict.ContainsKey(prefab))
+        {
+            Debug.LogError("Pool not exits: " + prefab.name);
+            return null;
+        }
+
+        GameObject obj = poolDict[prefab].Dequeue();
+        obj.transform.SetPositionAndRotation(pos, rot);
+        obj.SetActive(true);
+
+        if (obj.TryGetComponent(out IPoolable poolable))
+            poolable.OnSpawn();
+
+        poolDict[prefab].Enqueue(obj);
+        return obj;
+    }
+}
